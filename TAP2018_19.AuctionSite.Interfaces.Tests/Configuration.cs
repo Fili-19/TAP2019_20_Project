8e61c9d7025398c0e29fe372acc4a6b19dc15043
Filippo Formica <Fili_93@outlook.it>
2020-01-27 11:07:34 +0100

    Add Configuration Class

---
+++ TAP2018_19.AuctionSite.Interfaces.Tests/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace TAP2018_19.AuctionSite.Interfaces.Tests
+{
+    internal static class Conﬁguration
+    {
+        internal const string ConnectionString = null;
+        internal const string ImplementationAssembly = null;
+        internal const string ClockImplementationAssembly = null;
+    }
+}
