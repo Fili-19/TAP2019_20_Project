4f97ca0ace719e804e6e34bc5e9987dd7f13fcf6
Filippo Formica <Fili_93@outlook.it>
2020-01-27 10:31:37 +0100

    Add AuctionSiteNinjectModule Load method

--- TAP2019_20_Project/AuctionSiteNinjectModule.cs
+++ TAP2019_20_Project/AuctionSiteNinjectModule.cs
@@ -1,6 +1,9 @@
-﻿using System.Reflection;
+﻿using System.Data.Entity;
+using System.Reflection;
 using System.Security;
 using Ninject.Modules;
+using TAP2018_19.AuctionSite.Data;
+using TAP2018_19.AuctionSite.Interfaces;
 
 [assembly: AllowPartiallyTrustedCallers]
 
@@ -13,7 +16,12 @@ namespace TAP2019_20_Project
         [SecurityCritical]
         public override void Load()
         {
-            throw new System.NotImplementedException();
+            Database.SetInitializer<ASContext>(null);
+            this.Bind<ISiteFactory>().To<SiteFactory>();
+            this.Bind<ISite>().To<Site>();
+            this.Bind<IAuction>().To<Auction>();
+            this.Bind<ISession>().To<Session>();
+            this.Bind<IUser>().To<User>();
         }
     }
 }
