74a8d530b537af27a535b6dc4e593aca2b10a0e4
Filippo Formica <Fili_93@outlook.it>
2020-01-27 11:17:23 +0100

    add InstrumentedAuctionSiteTest statuc constructor

--- TAP2018_19.AuctionSite.Interfaces.Tests/InstrumentedAuctionSiteTest.cs
+++ TAP2018_19.AuctionSite.Interfaces.Tests/InstrumentedAuctionSiteTest.cs
@@ -1,10 +1,22 @@
-﻿using NUnit.Framework;
+﻿using Ninject;
+using NUnit.Framework;
+using TAP2018_19.AlarmClock.Interfaces;
 
 namespace TAP2018_19.AuctionSite.Interfaces.Tests
 {
     [TestFixture]
     public abstract class InstrumentedAuctionSiteTest
     {
-     
+        protected static readonly IAlarmClockFactory AnAlarmClockFactory;
+        protected static readonly ISiteFactory AnAuctionSiteFactory;
+
+        static InstrumentedAuctionSiteTest()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Conﬁguration.ClockImplementationAssembly);
+            AnAlarmClockFactory = kernel.Get<IAlarmClockFactory>();
+            kernel.Load(Conﬁguration.ImplementationAssembly);
+            AnAuctionSiteFactory = kernel.Get<ISiteFactory>();
+        }
     }
 }
