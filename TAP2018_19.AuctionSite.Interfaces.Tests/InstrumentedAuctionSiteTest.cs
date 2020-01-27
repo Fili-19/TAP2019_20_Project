564ecd576401c6bb682b9efbbbbcbf01682dae08
Filippo Formica <Fili_93@outlook.it>
2020-01-27 11:05:58 +0100

    add Setup_BadConnectionString_Throws

--- TAP2018_19.AuctionSite.Interfaces.Tests/InstrumentedAuctionSiteTest.cs
+++ TAP2018_19.AuctionSite.Interfaces.Tests/InstrumentedAuctionSiteTest.cs
@@ -1,7 +1,10 @@
-﻿namespace TAP2018_19.AuctionSite.Interfaces.Tests
+﻿using NUnit.Framework;
+
+namespace TAP2018_19.AuctionSite.Interfaces.Tests
 {
+    [TestFixture]
     public abstract class InstrumentedAuctionSiteTest
     {
-        
+     
     }
 }
--- TAP2018_19.AuctionSite.Interfaces.Tests/SiteFactoryBadConnectionTest.cs
+++ TAP2018_19.AuctionSite.Interfaces.Tests/SiteFactoryBadConnectionTest.cs
@@ -1,7 +1,14 @@
-﻿namespace TAP2018_19.AuctionSite.Interfaces.Tests
-{
+﻿using NUnit.Framework;
+
+namespace TAP2018_19.AuctionSite.Interfaces.Tests
+{   
+    [TestFixture]
     public class SiteFactoryBadConnectionTest : InstrumentedAuctionSiteTest
     {
-        
+        [Test]
+        public void Setup_BadConnectionString_Throws()
+        {
+            
+        }
     }
 }
